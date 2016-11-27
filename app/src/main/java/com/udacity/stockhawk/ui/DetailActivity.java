284e4215c68f83daf9dcfa697f31c771dda19d61
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-11-26 21:20:22 -0500

    feat: Open Detail Activity on click of stock items.

---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -0,0 +1,23 @@
+package com.udacity.stockhawk.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.udacity.stockhawk.R;
+
+/**
+ * Created by KarthicK on 11/26/2016.
+ *
+ * Detail Activity
+ * to show the Graph of selected stock's value over time.
+ */
+
+public class DetailActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}

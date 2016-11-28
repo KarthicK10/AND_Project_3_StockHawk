63fbf78fc7dea3103278dbe12b31ec6638627f16
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-11-27 20:58:04 -0500

    feat: Add a static widget launching application on click.

---
+++ app/src/main/java/com/udacity/stockhawk/widget/DetailWidgerProvider.java
@@ -0,0 +1,31 @@
+package com.udacity.stockhawk.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.ui.MainActivity;
+
+/**
+ * Created by KarthicK on 11/27/2016.
+ */
+
+public class DetailWidgerProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for(int appWidgetID : appWidgetIds){
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_detail);
+
+            //Create an Intent to launch MainActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget_text, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetID, views);
+        }
+    }
+}
